b60720ceaf1b89cac709f9ef8296b405411c51be
Luciano <lucianosesma36@gmail.com>
2025-04-10 19:11:57 -0300

    version1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import NavBar from "./components/NavBar"
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <h1>holaa</h1>
+    </div>
+    
+  <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
